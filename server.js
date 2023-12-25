e99657dd7900300380feef1ba2ce807a3749ed07
Blaster1111 <rudrashukla0711@gmail.com>
2023-12-26 01:00:51 +0530

    trial

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get('/',(req,res) =>{
     res.send("Hello node api")
 })
 
+app.get('/blog',(req,res)=>{
+    res.send("Hello Blog")
+})
+
+
 app.listen(5000,()=>{
     console.log("App is ruunning on port 3000")
 })
