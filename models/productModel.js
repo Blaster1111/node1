4c2a3df1f6b0024b8ce0ecfa28943e5bd0d5a8c0
Blaster1111 <rudrashukla0711@gmail.com>
2023-12-26 02:20:37 +0530

    Learnt MongoDB connection

---
+++ models/productModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+
+const productModel = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please Enter a product name"]
+        },
+        quantity: {
+            type: Number,
+            required: true,
+
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: false,
+        },
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Product = mongoose.model('Product',productSchema)
+module.exports = Product;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const mangoose = require("mongoose")
 const app = express()
 
 //routes
@@ -10,7 +11,21 @@ app.get('/blog',(req,res)=>{
     res.send("Hello Blog")
 })
 
+app.post('/product',(req,res)=>{
+    console.log(req.body)
+    res.send(req.body)
+    
+})
 
-app.listen(5000,()=>{
-    console.log("App is ruunning on port 3000")
+
+mangoose.
+connect('mongodb+srv://rudra:rudra1111@cluster0.saeci7g.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(()=>{
+    console.log("Connected to MongoDB")
+    app.listen(5000,()=>{
+        console.log("App is ruunning on port 3000")
+    })
+    
+}).catch(()=>{
+    console.log(error)
 })
